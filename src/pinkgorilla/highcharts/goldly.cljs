f224ad1ac46237424eb437f2e025f844c0ea2b12
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-06-30 16:57:33 -0500

    theme config via edn

--- src/pinkgorilla/highcharts/goldly.cljs
+++ src/pinkgorilla/highcharts/goldly.cljs
@@ -1,20 +1,15 @@
 (ns pinkgorilla.highcharts.goldly
   (:require
-   [re-frame.core :as rf]
-
    ; pinkie
    [pinkie.default-setup] ; side-effects
    [pinkie.pinkie :refer-macros [register-component]]
 
    ; helper fns
-   [pinkgorilla.highcharts.css :as css]
    [pinkgorilla.highcharts.core :refer [highchart highstock
                                         highchart-boxed highstock-boxed]]
 
 ;   
    ))
-
-(rf/dispatch [:css/add-components css/components css/config])
 
 (register-component :p/highchart highchart)
 (register-component :p/highstock highstock)
